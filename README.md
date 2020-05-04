74961eda42b194d981a473a60f552112cebe8597
Joanne <siroisjo@gmail.com>
2020-05-04 13:31:48 -0400

    Added Comments and Updated README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# IntroductionToCpp
+# Introduction To C++
+
+This repo is to hold files implemented in my youtube series: https://www.youtube.com/playlist?list=PLaBPUIXZ8s4BfLyUhFC0xToNsK6FHYzEj
+
+This series reviews the fundamentals of the C++ language.
--- part1/helloworld.cpp
+++ part1/helloworld.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include <iostream> // Header file library that lets you work with input/outputs
 
-using namespace std;
+using namespace std; // Allows you to use named for objects abd variables from the standard library
 
 int main() {
-    cout << "Hello World!";
+    cout << "Hello World!"; // Prints Hello World! to terminal
     return 0;
 }
